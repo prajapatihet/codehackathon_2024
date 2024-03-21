afe140d9b5ab1cdf50b9df78639999f59cd9f382
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-03-21 19:56:20 +0530

    fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:connectblooddonor/main.dart';
+import 'package:donorconnect/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
