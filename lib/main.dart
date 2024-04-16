de50a59b53f867ab099e3885429641715103ee9b
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-04-16 10:34:31 +0530

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ConnectBloodDonor',
+      title: 'ConnectDonor',
       onGenerateRoute: Routers.generateRoute,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- lib/src/views/getstart/onboarding.dart
+++ lib/src/views/getstart/onboarding.dart
@@ -12,18 +12,17 @@ class OnBoardingScreen extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
                 'Welcome to our Application',
                 textAlign: TextAlign.center,
                 style: GoogleFonts.montserrat(
-                  fontSize: 26,
+                  fontSize: 27,
                   fontWeight: FontWeight.w800,
                   letterSpacing: 1,
                 ),
@@ -35,10 +34,10 @@ class OnBoardingScreen extends StatelessWidget {
                 'Start sharing your life-saving moments and\n find nearby donors!',
                 textAlign: TextAlign.center,
                 style: GoogleFonts.roboto(
-                  fontSize: 13,
+                  fontSize: 14,
                   fontWeight: FontWeight.w500,
                   letterSpacing: 0.5,
-                  color: Colors.black.withOpacity(0.8),
+                  color: Colors.black.withOpacity(0.5),
                 ),
               ),
               SizedBox(
@@ -46,8 +45,8 @@ class OnBoardingScreen extends StatelessWidget {
               ),
               Image.asset(
                 'assets/images/onboarding.png',
-                width: width * 0.6,
-                height: height * 0.4,
+                width: width * 0.5,
+                height: height * 0.3,
               ),
               SizedBox(
                 height: height * 0.05,
